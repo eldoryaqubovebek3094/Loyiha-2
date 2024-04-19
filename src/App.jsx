c3aec6b80b3da4a0475d6e1430d0e77206f11562
eldoryaqubovebek3094 <148733238+eldoryaqubovebek3094@users.noreply.github.com>
2024-04-19 09:15:27 +0300

    giasdegiudg

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css';
 import Card from './components/Card';
+import Counter from './components/Counter';
 
 function App() {
   return (
     <div className="App">
+      <Counter/>
       <Card/>
     </div>
   );
